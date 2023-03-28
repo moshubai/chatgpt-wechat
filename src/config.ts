35c91cd1e4d077ea1df3670f5294e63195a1fc40
许占波 <hx@xuzhanbodeMacBook-Pro.local>
2023-03-28 20:40:49 +0800

    feat: ✈️first commit

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+import * as dotenv from "dotenv";
+import { IConfig } from "./interface";
+dotenv.config();
+
+export const config: IConfig = {
+  api: process.env.API || "https://api.openai.com",
+  openai_api_key: process.env.OPENAI_API_KEY || "123456789",
+  model: process.env.MODEL || "gpt-3.5-turbo",
+  // 聊中触发的关键词 
+  chatPrivateTriggerKeyword: process.env.CHAT_PRIVATE_TRIGGER_KEYWORD || "",
+  chatTriggerRule: process.env.CHAT_TRIGGER_RULE || "",
+  // 禁止群聊使用机器人
+  disableGroupMessage: process.env.DISABLE_GROUP_MESSAGE === "true",
+  temperature: process.env.TEMPERATURE ? parseFloat(process.env.TEMPERATURE) : 0.6,
+  // 避免敏感词
+  blockWords: process.env.BLOCK_WORDS?.split(",") || [],
+  // 连天敏感词
+  chatgptBlockWords: process.env.CHATGPT_BLOCK_WORDS?.split(",") || [],
+  // 一条消息的字数
+  singleMessageMaxSize: process.env.SINGLE_MESSAGE_MAX_SIZE || 5000
+};
+
---
+++ src/interface.ts
@@ -0,0 +1,18 @@
+import { ChatCompletionRequestMessage } from "openai";
+
+export interface IConfig {
+  api: string;
+  openai_api_key: string;
+  model: string;
+  chatTriggerRule: string;
+  disableGroupMessage: boolean;
+  temperature: number;
+  blockWords: string[];
+  chatgptBlockWords: string[];
+  chatPrivateTriggerKeyword: string;
+  singleMessageMaxSize: number;
+}
+export interface User {
+  username: string,
+  chatMessage: Array<ChatCompletionRequestMessage>,
+}
