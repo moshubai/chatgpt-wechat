af080d0f11da9ecee4c45b1e03ee41fbbe5c12b2
许占波 <hx@xuzhanbodeMacBook-Pro.local>
2023-04-01 09:56:30 +0800

    fix: 优化进入房间

--- src/interface.ts
+++ src/interface.ts
@@ -1,18 +1,18 @@
-import { ChatCompletionRequestMessage } from "openai";
+import { ChatCompletionRequestMessage } from 'openai'
 
 export interface IConfig {
-  api: string;
-  openai_api_key: string;
-  model: string;
-  chatTriggerRule: string;
-  disableGroupMessage: boolean;
-  temperature: number;
-  blockWords: string[];
-  chatgptBlockWords: string[];
-  chatPrivateTriggerKeyword: string;
-  singleMessageMaxSize: number;
+  api: string
+  openai_api_key: string
+  model: string
+  chatTriggerRule: string
+  disableGroupMessage: boolean
+  temperature: any
+  blockWords: string[]
+  chatgptBlockWords: string[]
+  chatPrivateTriggerKeyword: string
+  singleMessageMaxSize: number
 }
 export interface User {
-  username: string,
-  chatMessage: Array<ChatCompletionRequestMessage>,
+  username: string
+  chatMessage: Array<ChatCompletionRequestMessage>
 }
